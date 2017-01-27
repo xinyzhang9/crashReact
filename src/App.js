07bb7f3ae0b4dd94284eeaa5dd1d17c71810a268
Xinyang <xinyzhang9@gmail.com>
2017-01-27 01:35:07 -0800

    add projects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
@@ -26,11 +27,15 @@ class App extends Component {
         }
     ]});
   }
-
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects});
+  }
   render() {
     return (
       <div className="App">
-        My App
+        <AddProject addProject={this.handleAddProject.bind(this)} />
         <Projects projects={ this.state.projects }/>
       </div>
     );
---
+++ src/Components/AddProject.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newProject:{}
+    }
+  }
+  static defaultProps = {
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+
+  handleSumbit(e){
+    if(this.refs.title.value === ''){
+      alert('title is required');
+    }else{
+      this.setState({newProject:{
+        title: this.refs.title.value,
+        category: this.refs.category.value
+      }}, function(){
+        // console.log(this.state);
+        this.props.addProject(this.state.newProject);
+      });
+    }
+    e.preventDefault();
+  }
+  render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key={category} value={category}>{category}</option>
+    })
+    return (
+      <div>
+        <h3>Add Project </h3>
+        <form onSubmit={this.handleSumbit.bind(this)}>
+          <div>
+            <label>Title</label>
+            <input type='text' ref='title' />
+          </div>
+          <div>
+            <label>Category</label>
+            <select ref='category'>
+              {categoryOptions}
+            </select>
+          </div>
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
